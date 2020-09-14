18c3de7ef092f0ffbbc9b9148c8288e6e4306b8c
Sinyoung3016 <tasha0415@naver.com>
2020-09-14 11:23:41 +0900

    Modify C Problem and Solution problem : content solution : file name

--- C/solution/Main.java
+++ C/solution/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class solution {
+public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
