3bb92716267966be4f4eb15e375c6c02c5cd40e3
potionk <potionkr@gmail.com>
2020-09-15 02:19:58 +0900

    Rename java solution file name

--- A/solution/Main.java
+++ A/solution/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
--- I/solution/Main.java
+++ I/solution/Main.java
@@ -1,11 +1,9 @@
-package I;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
