3b92a025cdbb3fa8500376f01afe85a607084403
Yami <31057849+siontama@users.noreply.github.com>
2020-09-14 09:01:58 +0900

    solution 수정

--- D/solution/solution.cpp
+++ D/solution/solution.cpp
@@ -1,31 +1,61 @@
-﻿#define _USE_MATH_DEFINES
-
-#include<iostream>
-#include<cstdio>
-#include<cmath>
-#include<cstring>
-
-#include<algorithm>
-#include<functional>
-
-#include<string>
-#include<vector>
-#include<map>
+﻿#include<stdio.h>
 #include<queue>
-#include<stack>
-#include<tuple>
-
+#include<vector>
+#include<algorithm>
+#define INF 987654321;
 using namespace std;
-const int INF = numeric_limits<int>::max();
-const double pi = M_PI;
-
-int main()
-{
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		if (31 % (i + 1) == 1) {
-			cout << i << "\n";
+int N, M, X, Y;
+int Dist[501];//우리집에서 이웃집 까지의 거리
+vector<pair<int, int>> v[501];//pair<b,거리>[a]
+priority_queue<pair<int, int>> pq;//pair< 가중치와 현재 집 위치 
+int main() {
+	scanf("%d %d %d %d", &N, &M, &X, &Y);
+	for (int i = 0; i < M; i++) {
+		int a, b, len;
+		scanf("%d %d %d", &a, &b, &len);
+		v[a].push_back(make_pair(b, len));
+		v[b].push_back(make_pair(a, len));
+	}
+	//다익스트라
+	for (int i = 0; i < 501; i++) {
+		Dist[i] = INF;
+	}
+	Dist[Y] = 0;
+	pq.push(make_pair(0, Y));
+	while (!pq.empty()) {
+		int cur = pq.top().second;
+		int cCost = -pq.top().first;
+		pq.pop();
+		for (int i = 0; i < v[cur].size(); i++) {
+			int next = v[cur][i].first;
+			int nCost = v[cur][i].second;
+			if (cCost + nCost < Dist[next]) {
+				Dist[next] = cCost + nCost;
+				pq.push(make_pair(-Dist[next], next));
+			}
 		}
 	}
+	sort(Dist, Dist + 501);
+	for (int i = 0; i < N; i++) {
+		Dist[i] *= 2;//왕복이므로
+	}
+	int temp = X;
+	int Date = 1;
+	for (int i = 0; i < N; i++) {
+		if (temp - Dist[i] > 0) {
+			temp -= Dist[i];
+		}
+		else {
+			if (Dist[i] >= X) {
+				Date = -1;
+				break;
+			}
+			else {
+				temp = X;
+				Date++;
+				i--;
+			}
+		}
+	}
+	printf("%d", Date);
 }
